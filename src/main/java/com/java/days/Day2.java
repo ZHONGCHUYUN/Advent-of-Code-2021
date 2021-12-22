6053364d15d9ab1d3fcb3efe38dab754d91efa15
Chuyun <zhongchuyun27@gmail.com>
2021-12-22 15:19:44 +0000

    day2 part2

--- src/main/java/com/java/days/Day2.java
+++ src/main/java/com/java/days/Day2.java
@@ -27,4 +27,30 @@ public class Day2 {
         }
         return h*v;
     }
+
+    // answer: 1568138742
+    public int solution_part2() {
+        List<String> list = InputUtils.asStringList("/Users/chuyunzhong/Desktop/Advent-of-Code-2021/src/main/java/com/java/resources/day2.txt");
+        int h = 0;
+        int v = 0;
+        int aim = 0;
+        for (String line: list) {
+            String[] temp = line.split(" ");
+            switch(temp[0]) {
+                case "forward":
+                    int val = Integer.parseInt(temp[1]);
+                    h += val;
+                    v += val * aim;
+                    break;
+                case "down":
+                    aim += Integer.parseInt(temp[1]);
+                    break;
+                case "up":
+                    aim -= Integer.parseInt(temp[1]);
+                default :
+                    break;
+            }
+        }
+        return h*v;
+    }
 }
