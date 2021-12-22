ee09aaee0ddd7d0078088eec28b9e30b59ed8761
Chuyun <zhongchuyun27@gmail.com>
2021-12-22 13:54:30 +0000

    day1 part2

--- src/com/java/days/Day1.java
+++ src/com/java/days/Day1.java
@@ -7,8 +7,10 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Day1 {
+    private List<Integer> list;
+
     public int solution_part1() {
-        List<Integer> list = new ArrayList<>();
+        list = new ArrayList<>();
         try {
             Scanner scanner = new Scanner(new File("/Users/chuyunzhong/Desktop/Advent-of-Code-2021/src/com/java/resources/day1.txt"));
             while (scanner.hasNextLine()) {
@@ -21,6 +23,33 @@ public class Day1 {
         for (int i = 1; i<list.size(); i++) {
             if (list.get(i) > list.get(i-1)) {
                 result ++ ;
+            }
+        }
+        return result;
+    }
+
+    // answer: 1728
+    public int solution_part2() {
+        list = new ArrayList<>();
+        try {
+            Scanner scanner = new Scanner(new File("/Users/chuyunzhong/Desktop/Advent-of-Code-2021/src/com/java/resources/day1_2.txt"));
+            while (scanner.hasNextLine()) {
+                list.add(Integer.parseInt(scanner.nextLine()));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        int result = 0;
+        int pre = 0;
+        for (int i = 0; i<list.size()-2; i++) {
+            if (i == 0) {
+                pre = list.get(i) + list.get(i+1) + list.get(i+2);
+            } else {
+                int cur = list.get(i) + list.get(i+1) + list.get(i+2);
+                if ( cur > pre) {
+                    result ++ ;
+                }
+                pre = cur;
             }
         }
         return result;
