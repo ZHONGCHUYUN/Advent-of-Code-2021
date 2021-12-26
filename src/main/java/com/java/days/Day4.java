cd891b1cf7f372670ac4ba40acc998ee34310ef9
Chuyun <zhongchuyun27@gmail.com>
2021-12-26 17:43:59 +0000

    day4-part2

--- src/main/java/com/java/days/Day4.java
+++ src/main/java/com/java/days/Day4.java
@@ -32,7 +32,7 @@ public class Day4 {
                 for (int i = 0; i<boardSize; i++) {
                     for (int j = 0; j<boardSize; j++) {
                         if (boards.get(k).get(i).get(j) == target) {
-                            boards.get(k).get(i).set(j, 0);
+                            boards.get(k).get(i).set(j, -1);
                             verticals[k][j] ++ ;
                             horizontal[k][i] ++ ;
                             if (i == j) diagonal[k] ++ ;
@@ -58,11 +58,69 @@ public class Day4 {
         return 0;
     }
 
+    public int solution_part2() {
+        List<String> list = InputUtils.asStringList("/Users/chuyunzhong/Desktop/Advent-of-Code-2021/src/main/java/com/java/resources/day4.txt");
+        List<Integer> targets = List.of(list.get(0).split(",")).stream().map(Integer::parseInt).collect(Collectors.toList());
+        List<List<List<Integer>>> boards = new ArrayList<>();
+        List<List<Integer>> board = new ArrayList<>();
+        for (int i = 2; i<list.size(); i++) {
+            if (list.get(i).length() == 0) {
+                boards.add(board);
+                board = new ArrayList<>();
+            } else {
+                board.add(List.of(list.get(i).trim().split("\\s+")).stream().map(Integer::parseInt).collect(Collectors.toList()));
+            }
+        }
+        int boardSize = board.size();
+        int numOfBoard = boards.size();
+        int[][] verticals = new int[numOfBoard][boardSize];
+        int[][] horizontal = new int[numOfBoard][boardSize];
+        int[] diagonal = new int[numOfBoard];
+        int[] antiDiagonal = new int[numOfBoard];
+        int result = 0;
+        boolean[] visited = new boolean[numOfBoard];
+        for (int target: targets) {
+            for (int k = 0; k<numOfBoard; k++) {
+                for (int i = 0; i<boardSize; i++) {
+                    for (int j = 0; j<boardSize; j++) {
+                        if (boards.get(k).get(i).get(j) == target) {
+                            boards.get(k).get(i).set(j, -1);
+                            verticals[k][j] ++ ;
+                            horizontal[k][i] ++ ;
+                            if (i == j) diagonal[k] ++ ;
+                            if (i+j+1 == boardSize) antiDiagonal[k] ++ ;
+                            if (isVerticalLine(verticals[k], boardSize) && !visited[k]) {
+                                int sum = calculateUnmarkedSum(boards.get(k));
+                                result = sum * target;
+                                visited[k] = true;
+                            } else if (isHorizontalLine(horizontal[k], boardSize) && !visited[k]) {
+                                int sum = calculateUnmarkedSum(boards.get(k));
+                                result = sum * target;
+                                visited[k] = true;
+                            } else if (diagonal[k] == boardSize && !visited[k]) {
+                                int sum = calculateUnmarkedSum(boards.get(k));
+                                result = sum * target;
+                                visited[k] = true;
+                            } else if (antiDiagonal[k] == boardSize && !visited[k]) {
+                                int sum = calculateUnmarkedSum(boards.get(k));
+                                result = sum * target;
+                                visited[k] = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
     private int calculateUnmarkedSum(List<List<Integer>> list) {
         int sum = 0;
         for (int i = 0; i<list.size(); i++) {
             for (int j = 0; j<list.get(0).size(); j++) {
-                sum += list.get(i).get(j);
+                if (list.get(i).get(j) != -1) {
+                    sum += list.get(i).get(j);
+                }
             }
         }
         return sum;
@@ -81,4 +139,5 @@ public class Day4 {
         }
         return false;
     }
+
 }
